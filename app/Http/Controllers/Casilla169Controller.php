5e258e12e2c823c90d014fdd1afc65fe39575abf
jere11d <jeremias11dic@gmail.com>
2023-01-07 01:19:15 -0600

    Update Casilla169Controller.php

--- app/Http/Controllers/Casilla169Controller.php
+++ app/Http/Controllers/Casilla169Controller.php
@@ -2,9 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Casilla169s;
+use Exception;
 use Illuminate\Http\Request;
 
-class Casilla169Controller extends Controller
+class Casilla169sController extends Controller
 {
     //
+    public function addCasilla169s(Request $request)
+    {
+        try {
+            $casilla169s = new Casilla169s();
+            $casilla169s->nit_sujeto = $request->nit_sujeto;
+            $casilla169s->fecha_emision = $request->fecha_emision;
+            $casilla169s->tipo_documento = $request->tipo_documento;
+            $casilla169s->numero_resolucion = $request->numero_resolucion;
+            $casilla169s->serie_documento = $request->serie_documento;
+            $casilla169s->numero_documento = $request->numero_documento;
+            $casilla169s->monto_sujeto = $request->monto_sujeto;
+            $casilla169s->monto_percepcion = $request->monto_percepcion;
+            $casilla169s->dui_sujeto = $request->dui_sujeto;
+            $casilla169s->numero_anexo = $request->numero_anexo;
+            $casilla169s->user_id = $request->user_id; // Datos de la sesion del usuario logeado.
+            $result = $casilla169s->save();
+
+            return response()->json($result, 200);
+
+        } catch (Exception $e) {
+            return response()->json($e->getMessage(), 403);
+        }
+    }
+
+    public function updateCasilla169s(Request $request, $id)
+    {
+        try {
+            $casilla169s = Casilla169s::find($id);
+            $casilla169s->nit_sujeto = $request->nit_sujeto;
+            $casilla169s->fecha_emision = $request->fecha_emision;
+            $casilla169s->tipo_documento = $request->tipo_documento;
+            $casilla169s->numero_resolucion = $request->numero_resolucion;
+            $casilla169s->serie_documento = $request->serie_documento;
+            $casilla169s->numero_documento = $request->numero_documento;
+            $casilla169s->monto_sujeto = $request->monto_sujeto;
+            $casilla169s->monto_percepcion = $request->monto_percepcion;
+            $casilla169s->dui_sujeto = $request->dui_sujeto;
+            $casilla169s->numero_anexo = $request->numero_anexo;
+            $result = $casilla169s->save();
+
+            return response()->json($result, 200);
+
+        } catch (Exception $e) {
+            return response()->json($e->getMessage(), 403);
+        }
+    }
+
+    public function deleteCasilla169s($id)
+    {
+        try {
+            $casilla169s = Casilla169s::find($id);
+            $result = $casilla169s->delete();
+
+            return response()->json($result, 200);
+        } catch (Exception $e) {
+            return response()->json($e->getMessage(), 403);
+        }
+    }
+
+
 }
