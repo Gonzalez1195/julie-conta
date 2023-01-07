a6fdde2ca3c2eca3e436127d5fff1b1a28ba9eb3
jere11d <jeremias11dic@gmail.com>
2023-01-07 01:18:05 -0600

    Update Casilla163Controller.php

--- app/Http/Controllers/Casilla163Controller.php
+++ app/Http/Controllers/Casilla163Controller.php
@@ -2,9 +2,69 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Casilla163s;
+use Exception;
 use Illuminate\Http\Request;
 
-class Casilla163Controller extends Controller
+class Casilla163sController extends Controller
 {
     //
+    public function addCasilla163s(Request $request)
+    {
+        try {
+            $casilla163s = new Casilla163s();
+            $casilla163s->nit_agente = $request->nit_agente;
+            $casilla163s->fecha_emision = $request->fecha_emision;
+            $casilla163s->tipo_documento = $request->tipo_documento;
+            $casilla163s->serie_documento = $request->serie_documento;
+            $casilla163s->numero_documento = $request->numero_documento;
+            $casilla163s->monto_sujeto = $request->monto_sujeto;
+            $casilla163s->monto_percepcion = $request->monto_percepcion;
+            $casilla163s->dui_agente = $request->dui_agente;
+            $casilla163s->numero_anexo = $request->numero_anexo;
+            $casilla163s->user_id = $request->user_id; // Datos de la sesion del usuario logeado.
+            $result = $casilla163s->save();
+
+            return response()->json($result, 200);
+
+        } catch (Exception $e) {
+            return response()->json($e->getMessage(), 403);
+        }
+    }
+
+    public function updateCasilla163s(Request $request, $id)
+    {
+        try {
+            $casilla163s = Casilla163s::find($id);
+            $casilla163s->nit_agente = $request->nit_agente;
+            $casilla163s->fecha_emision = $request->fecha_emision;
+            $casilla163s->tipo_documento = $request->tipo_documento;
+            $casilla163s->serie_documento = $request->serie_documento;
+            $casilla163s->numero_documento = $request->numero_documento;
+            $casilla163s->monto_sujeto = $request->monto_sujeto;
+            $casilla163s->monto_percepcion = $request->monto_percepcion;
+            $casilla163s->dui_agente = $request->dui_agente;
+            $casilla163s->numero_anexo = $request->numero_anexo;
+            $result = $casilla163s->save();
+
+            return response()->json($result, 200);
+
+        } catch (Exception $e) {
+            return response()->json($e->getMessage(), 403);
+        }
+    }
+
+    public function deleteCasilla163s($id)
+    {
+        try {
+            $casilla163s = Casilla163s::find($id);
+            $result = $casilla163s->delete();
+
+            return response()->json($result, 200);
+        } catch (Exception $e) {
+            return response()->json($e->getMessage(), 403);
+        }
+    }
+
+
 }
