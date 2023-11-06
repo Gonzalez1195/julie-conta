b3919c57f0d27b3f3836262437521ecbd4083dd1
Gonzalez1195 <78380222+Gonzalez1195@users.noreply.github.com>
2023-11-06 01:04:50 +0100

    Update contribuyentesForm.blade.php

--- resources/views/julie_conta/anexos/contribuyentesForm.blade.php
+++ resources/views/julie_conta/anexos/contribuyentesForm.blade.php
@@ -10,13 +10,13 @@
                     <div class="row page-titles mx-0">
                         <div class="col-sm-6 p-md-0">
                             <div class="welcome-text">
-                                <h4>Editar Contribuyente Registro</h4>
+                                <h4>Editar contribuyente registro</h4>
                             </div>
                         </div>
                         <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a href="javascript:void(0)">Editar</a></li>
-                                <li class="breadcrumb-item active"><a href="javascript:void(0)">Contribuyente Registro</a></li>
+                                <li class="breadcrumb-item active"><a href="javascript:void(0)">Contribuyente registro</a></li>
                             </ol>
                         </div>
                     </div>
@@ -25,7 +25,7 @@
                         <div class="col-xl-12 col-lg-12">
                             <div class="card">
                                 <div class="card-header">
-                                    <h4 class="card-title">Editar Usuario</h4>
+                                    <h4 class="card-title">Editar usuario</h4>
                                 </div>
                                 <div class="card-body">
                                     <div class="basic-form form-validation">
